89a6bbc0efe118dafe26deef2a221742c8ac288c
Chris Hall <chris@clockwork-language.co.uk>
2014-04-15 13:07:07 +0100

    Start flattening (removing configuration for centos etc.).

--- puphpet/shell/update-puppet.sh
+++ puphpet/shell/update-puppet.sh
@@ -7,38 +7,20 @@ RELEASE=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" RELEASE)
 CODENAME=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" CODENAME)
 
 if [[ ! -f /.puphpet-stuff/update-puppet ]]; then
-    if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then  
-        echo "Downloading http://apt.puppetlabs.com/puppetlabs-release-${CODENAME}.deb"
-        wget --quiet --tries=5 --connect-timeout=10 -O "/.puphpet-stuff/puppetlabs-release-${CODENAME}.deb" "http://apt.puppetlabs.com/puppetlabs-release-${CODENAME}.deb"
-        echo "Finished downloading http://apt.puppetlabs.com/puppetlabs-release-${CODENAME}.deb"
+    echo "Downloading http://apt.puppetlabs.com/puppetlabs-release-${CODENAME}.deb"
+    wget --quiet --tries=5 --connect-timeout=10 -O "/.puphpet-stuff/puppetlabs-release-${CODENAME}.deb" "http://apt.puppetlabs.com/puppetlabs-release-${CODENAME}.deb"
+    echo "Finished downloading http://apt.puppetlabs.com/puppetlabs-release-${CODENAME}.deb"
 
-        dpkg -i "/.puphpet-stuff/puppetlabs-release-${CODENAME}.deb" >/dev/null
+    dpkg -i "/.puphpet-stuff/puppetlabs-release-${CODENAME}.deb" >/dev/null
 
-        echo "Running update-puppet apt-get update"
-        apt-get update >/dev/null
-        echo "Finished running update-puppet apt-get update"
+    echo "Running update-puppet apt-get update"
+    apt-get update >/dev/null
+    echo "Finished running update-puppet apt-get update"
 	
-        echo "Updating Puppet to version 3.4.3"
-	apt-get install -y puppet=3.4.3-1puppetlabs1 puppet-common=3.4.3-1puppetlabs1 >/dev/null
-	echo "Finished updating puppet to version: 3.4.3"
+    echo "Updating Puppet to version 3.4.3"
+    apt-get install -y puppet=3.4.3-1puppetlabs1 puppet-common=3.4.3-1puppetlabs1 >/dev/null
+    echo "Finished updating puppet to version: 3.4.3"
 	
-	touch /.puphpet-stuff/update-puppet
-        echo "Created empty file /.puphpet-stuff/update-puppet"
-    elif [ "${OS}" == 'centos' ]; then
-        echo "Downloading http://yum.puppetlabs.com/el/${RELEASE}/products/x86_64/puppetlabs-release-6-7.noarch.rpm"
-        yum -y --nogpgcheck install "http://yum.puppetlabs.com/el/${RELEASE}/products/x86_64/puppetlabs-release-6-7.noarch.rpm" >/dev/null
-        echo "Finished downloading http://yum.puppetlabs.com/el/${RELEASE}/products/x86_64/puppetlabs-release-6-7.noarch.rpm"
-
-        echo "Running update-puppet yum update"
-        yum -y update >/dev/null
-        echo "Finished running update-puppet yum update"
-
-        echo "Installing/Updating Puppet to latest version"
-        yum -y install puppet >/dev/null
-        PUPPET_VERSION=$(puppet help | grep 'Puppet v')
-        echo "Finished installing/updating puppet to latest version: ${PUPPET_VERSION}"
-
-        touch /.puphpet-stuff/update-puppet
-        echo "Created empty file /.puphpet-stuff/update-puppet"
-    fi
+    touch /.puphpet-stuff/update-puppet
+    echo "Created empty file /.puphpet-stuff/update-puppet"
 fi
